2314f9a582f19b29bba9adecf2cc13939c7603e3
Carlos E Scarlato <cescarlato@yahoo.com.br>
2022-12-08 18:31:25 -0300

    Exemplos aula 1

---
+++ app.js
@@ -0,0 +1,83 @@
+// // // idades = [30, 35, 28]
+// // // nomes = ["Ana", "Julia", "Leonardo"]
+// // // faculdade = [false, true, true]
+
+// // // funcionarios = [nome, idades, faculdade]
+
+// // //  function eMaiorQue10(value) {
+// // //      return value >= 10;
+// // //  }
+
+// // // var filtrado = numeros.filter(eMaiorQue10);
+// // // // filtrado é[12, 130, 44]
+
+// // // console.log(filtrado)
+
+
+// // const notas1 = [10, 6.5, 8, 7.5]
+// // const notas2 = [9, 6, 6]
+// // const notas3 = [8.5, 9.5]
+
+
+// // const notasGerais = [notas1, notas2, notas3]
+
+// // let media = 0
+
+// // for (let i = 0; i < notasGerais.length; i++) {
+// //     for (let j = 0; j < notasGerais[i].length; j++) {
+// //         media += notasGerais[i][j] / notasGerais[i].length;
+// //     }
+// // }
+
+// // media = media / notasGerais.length
+
+// // console.log(media)
+
+// // teste()
+
+// // function teste() {
+// //     return 10
+// // }
+
+// // let TexteDeQuebraDeLinha = "Oi, tudo bem? Eu sou o Leonardo, esse texto é longo mesmo para podermos testar a quebra de linha"
+
+// // array = []
+// // array.push(1)
+// // console.log
+
+// // let PalavraSimples = ["oi", "ok"]
+
+// // for (let i = 0; i < PalavraSimples.length; i++) {
+// //     const element = PalavraSimples[i];
+
+// // }
+
+// // tipo Number
+
+// const meuNumero = 3;
+
+// const primeiroNumero = 1;
+// const segundoNumero = 2;
+
+// const operacaoMatematica = primeiroNumero + segundoNumero;
+
+// // console.log(operacaoMatematica);
+
+// // ponto flutuante
+
+// const numeroPontoFlutuante = 3.3;
+// const pontoFlutuanteSemZero = .5;
+
+// const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+// console.log(novaOperacao);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
